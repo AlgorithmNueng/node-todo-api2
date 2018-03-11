1f0c8ccf4928c107348ea7c9fd92159f69f5f938
Algorithm Nueng <mynung.comsci@gmail.com>
2018-03-12 00:17:20 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID} = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -26,6 +27,22 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id, function(err, todo) {
+    if (err) return res.status(400).send();
+    // if (!todo) return res.status(404).send();
+
+    res.send(todo);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Start on port 3000');
