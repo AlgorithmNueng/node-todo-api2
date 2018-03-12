2a114e5ac28290b8e8b71df91e88bff4ed6c8327
Algorithm Nueng <mynung.comsci@gmail.com>
2018-03-12 07:35:32 +0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID} = require('mongodb');
@@ -54,7 +55,32 @@ app.delete('/todos/:id', (req, res) => {
     if (err) return res.status(400).send();
     if (!doc) return res.status(404).send();
 
-    res.send(doc)
+    res.send({doc})
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+      body.completed = false;
+      body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
