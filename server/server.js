2723e71a953655a17a6e478a4ce5bbec23a0a913
Algorithm Nueng <mynung.comsci@gmail.com>
2018-03-13 07:42:06 +0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -84,10 +84,25 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Start on port 3000');
 });
 
+
+// POST /todos
 module.exports = { app };
 
 // NOTE: connect mongoose -> create Schema -> Create model -> Action etc.
